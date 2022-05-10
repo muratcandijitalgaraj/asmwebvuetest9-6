57ae839f406e149fe23465f7d12e49f1a2b1d0c2
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-05-10 17:03:20 +0300

    almost there

--- src/components/randevuAkis/flow2/Dropdown.vue
+++ src/components/randevuAkis/flow2/Dropdown.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    @click="handleCircle(item)"
+    @click="handleClick"
     class="dropdownItem d-flex align-items-center justify-content-start"
   >
     <div :class="{ chosenCircle: isCircleChosen }" class="circle"></div>
@@ -21,6 +21,66 @@ const props = defineProps({
   //   dropdownData: { required: true, type: Array },
   //   modelValue: { required: true, type: Number },
 });
+
+const emit = defineEmits(["update:modelValue"]);
+
+const isCheck = ref(false);
+const isClicked = ref(false);
+
+// const handle = ref(false);
+const handleClick = async () => {
+  await emit("update:modelValue", props.data);
+
+  if (props.modelValue == props.data) {
+    isCheck.value = true;
+  } else {
+    isCheck.value = false;
+  }
+  console.log(props.data);
+  changeBorderRadius();
+};
+
+const isCircleChosen = computed(() => {
+  if (isCheck.value && props.data === props.modelValue) {
+    return true;
+  }
+
+  return false;
+});
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.dropdownItem {
+  max-width: 500px;
+  width: auto;
+  height: 70px;
+  background: #ffffff;
+  /* Boxx Shadow */
+  box-shadow: 0px 1px 3px rgba(42, 49, 55, 0.11);
+  padding-left: 1rem;
+  border-top: 1px solid #f3f3f3;
+}
+.dropdownText {
+  /* text02 */
+  font-family: "Nunito Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 140%;
+
+  /* identical to box height, or 21px */
+
+  /* Primary */
+  color: #3c4e69;
+}
+.circle {
+  width: 20px;
+  height: 20px;
+  border: 1.3px solid #e1e1e1;
+  border-radius: 50%;
+  margin-right: 10px;
+}
+.chosenCircle {
+  background: #32a5df;
+}
+</style>
