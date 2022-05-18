8c65dcfa19b6fbcc45acca95ef83c237e49c8417
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-05-18 14:44:05 +0300

    ready for toplantı2

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,53 +1,7 @@
 <template>
-  <!-- Button trigger modal -->
-  <button
-    type="button"
-    class="btn btn-primary"
-    data-bs-toggle="modal"
-    data-bs-target="#exampleModal"
-  >
-    Launch demo modal
-  </button>
-
-  <!-- Modal -->
-  <div
-    class="modal fade"
-    id="exampleModal"
-    tabindex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
-        <div class="modal-body">...</div>
-        <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
-            Close
-          </button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
-    </div>
-  </div>
+  <div class="main"></div>
 </template>
 
-<script setup>
-import { ref } from "vue";
-import Modal from "./Playground2.vue";
-const isOpen = ref(false);
-</script>
+<script setup></script>
 
-<style></style>
+<style lang="scss" scoped></style>
